c49f8da476bcb76ad04ddc4a7ce19cc100de0496
julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>
2023-01-26 10:27:14 +0100

    fixup! Adapt native tooltip for new structure of NcAvatar
    Signed-off-by: julia.kirschenheuter <julia.kirschenheuter@nextcloud.com>

--- apps/files_sharing/src/components/SharingEntry.vue
+++ apps/files_sharing/src/components/SharingEntry.vue
@@ -26,7 +26,6 @@
 			:is-no-user="share.type !== SHARE_TYPES.SHARE_TYPE_USER"
 			:user="share.shareWith"
 			:display-name="share.shareWithDisplayName"
-			:tooltip-message="share.type === SHARE_TYPES.SHARE_TYPE_USER ? share.shareWith : ''"
 			:menu-position="'left'"
 			:url="share.shareWithAvatar" />
 
