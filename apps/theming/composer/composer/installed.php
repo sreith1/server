dadfcdab2d4df92f455a9ee8d33339c8574d9a0d
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-02-10 08:53:28 +0100

    perf(autoloading): Add authoritative autoloader for theming
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ apps/theming/composer/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '524db1c6dcc9eab8827f465375fc22854d7fee54',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '524db1c6dcc9eab8827f465375fc22854d7fee54',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
